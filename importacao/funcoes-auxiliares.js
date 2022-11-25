f3efbdaa539b2d8a21c27c2ee4c2547e35beb27e
Alberto Domingues <94317971+albertodomingues81@users.noreply.github.com>
2022-11-25 15:48:18 +0000

    Initial commit

---
+++ importacao/funcoes-auxiliares.js
@@ -0,0 +1,13 @@
+
+//Desvendando as Funções "gets" e "print" dos Desafios de Código
+
+function gets() {
+  return 10;
+}
+
+function print() {
+  console.log(texto);
+}
+
+
+module.exports = { gets, print };
